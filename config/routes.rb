29df3fac38f2014d9e2838a42110fb217aaf5822
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-08-07 22:33:48 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   get 'perfumes/index'
   root to: "perfumes#index"
-  resources :perfumes, only: [:destroy]
   resources :users, only: [:edit, :update]
 end
