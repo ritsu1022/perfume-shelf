53bf4d3d93943500c760cdb03348ef570d17c3fa
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-08-08 22:12:40 +0900

    active hashアソシエーション設定

--- app/models/perfume.rb
+++ app/models/perfume.rb
@@ -4,4 +4,9 @@ class Perfume < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :type
+
+  validates :type_id, numericality: { other_than: 1 }
 end
