93eb3109bdd520e13ea77be5dec4b9c2133d8c96
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-08-19 23:11:48 +0900

    usersコントローラー、showアクションルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'perfumes/index'
   root to: "perfumes#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update, :show]
   resources :perfumes, only: [:new, :create, :show, :edit, :update, :destroy]
 end
