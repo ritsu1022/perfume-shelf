79287b1e919ac9e75e846a5772e983ce4ebe1f91
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-09-10 02:15:05 +0900

    imageカラム追加時、ファイルの自動更新分

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_06_142301) do
+ActiveRecord::Schema.define(version: 2023_09_08_144102) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2023_08_06_142301) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.json "image"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
